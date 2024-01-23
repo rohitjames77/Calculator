9d29f6354df98688e83ef529ebac055a46dc1efa
rohit james <rohitjames77@outlook.com>
2024-01-23 18:24:44 +0530

    add fix for event bubbling and result display format

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ let inputArr =[];
 
 // Event for capturing input 
 let display = document.querySelector('.display');
-let eventParent = document.querySelector('.outerContainer');
+let eventParent = document.querySelector('.inputParent');
 eventParent.addEventListener('click',captureEvent)
 function captureEvent(event){
   let input= event.target.innerHTML;
   inputArr.push(input);       
-  display.innerHTML= inputArr;
+  display.innerHTML= inputArr.join('');
   if (event.target.className = "numInputsButtons"){
     numInput.push(event.target.innerHTML)
     console.log(numInput);
