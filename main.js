f23165714cc20f5e07fbfeb7d8aa099f3239a54a
rohit james <rohitjames77@outlook.com>
2024-01-26 18:19:01 +0530

    Fix innerHtml to textContent

--- main.js
+++ main.js
@@ -4,31 +4,43 @@ let mathSign=[];
 let inputArr =[];
 
 
-// Event for capturing input 
+// Event for capturing input from numberInput Div............................ 
+
 let display = document.querySelector('.display');
-let eventParent = document.querySelector('.inputParent');
-eventParent.addEventListener('click',captureEvent)
-function captureEvent(event){
-  let input= event.target.innerHTML;
-  inputArr.push(input);       
-  display.innerHTML= inputArr.join('');
-  
-  if (event.target.className = "numInputsButtons"){
-    numInput.push(event.target.innerHTML)
-    console.log(numInput);
+let eventParent1 = document.querySelector('.numberInput');
+eventParent1.addEventListener('click',captureEvent1)
+function captureEvent1(event){
+  if (event.target.className === "numInputsButtons"){
+  let input1= event.target.textContent ;
+  inputArr.push(input1);       
+  display.textContent = inputArr.join('');
+  event.stopPropagation();
+  console.log(inputArr);
+
+
+  collectInput();
   }
   }
+// Event for capturing input from mathOpInput Div............................
+  let eventParent2 = document.querySelector('.mathOpInput');
+  eventParent2.addEventListener('click',captureEvent2)
+  function captureEvent2(event){
+    if (event.target.className === "MathOpButtons"){
+    let input2= event.target.textContent ;
+    mathSign.push(input2);       
+    console.log(mathSign);
+    }
+
+    // collectInput();
+    }
+
+  function collectInput (inputArr,mathSign){
+    console.log(inputArr);
+    console.log(mathSign);
+
+  }
 
 
-
-  calculation()
-
-
-function calculation(input){
-
-
-
-}
 
 
     
