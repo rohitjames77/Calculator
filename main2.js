e46e8613c687705db2d9320d38fc8bca38ac70d4
rohit james <rohitjames77@outlook.com>
2024-02-02 23:49:49 +0530

    Fix updateDisplay function

--- main2.js
+++ main2.js
@@ -3,27 +3,36 @@
 
 let display = document.querySelector('.display');
 let eventParent1 = document.querySelector('.inputParent');
-eventParent1.addEventListener('click',captureEvent1,updateDisplay)
+eventParent1.addEventListener('click',captureEvent1)
 function captureEvent1(event){
-  let inputArr1 =[];
-  let inputArr2=[];
   let input = event.target.textContent;
-  if (event.target.className === "MathOpButtons"){
-    inputArr2.push(input);
-    console.log(inputArr2);
-  }else {
-    inputArr1.push(input);
-    console.log(inputArr1);
-
+     updateDisplay(input);   
   }
-updateDisplay(input);   
-  }
-   
-let num1;
-let num2;
-let opSign;
-
   
+  let inputArr = [];
+function updateDisplay(input){ 
+  inputArr.push(input);       
+  display.textContent = inputArr.join('');
+  console.log(inputArr);
+
+sortInput(inputArr);
+}
+
+function sortInput(inputArr){
+
+
+
+
+
+
+
+  // operate(num1,num2,opSign);
+}
+
+
+
+
+
 function operate (num1,num2,opSign){
 if (opSign === "+"){
   let result = num1 + num2;
@@ -39,20 +48,8 @@ if (opSign === "+"){
   console.log(result);
 }
 
-updateDisplay()
 }
 
-
-operate();
-
-
-
-function updateDisplay(input){
-display.textContent= input;
-
-
-
-}
 
 
 
