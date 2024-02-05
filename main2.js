ca86f0292272cd007c527008d538e79b120f019d
rohitjames <rohitjames77@outlook.com>
2024-02-05 19:48:57 +0530

    Fix SortInput

--- main2.js
+++ main2.js
@@ -1,47 +1,45 @@
 
 
 
+let inputArr = [];
 let display = document.querySelector('.display');
 let eventParent1 = document.querySelector('.inputParent');
 eventParent1.addEventListener('click',captureEvent1)
 function captureEvent1(event){
-  let input = event.target.textContent;
-     updateDisplay(input);   
-  }
-  
-  let inputArr = [];
-
- //populate display Div with  
-function updateDisplay(input){ 
-  inputArr.push(input);       
-  display.textContent = inputArr.join('');
-  console.log(inputArr);
-
-sortInput(inputArr);
-}
-
+  let input = event.target.textContent;  
+     inputArr.push(input);
+     inputArr.join('');
+     sortInput(inputArr);
+    }
+    
 function sortInput(inputArr){
   let opIndex= inputArr.findIndex(element =>['+','-','x','/'].includes(element));
   
   if (opIndex !== -1){
      let num1 = inputArr.slice(0,opIndex).join('');
      console.log(num1);
-     let num2 = inputArr.slice(opIndex +1,-1).join('');
+
+     let num2 = inputArr.slice(opIndex +1).join('');
      console.log(num2);
 
      let opSign = inputArr[opIndex];
      console.log(opSign);
-
+   
+     updateDisplay(num1,num2,opSign);
   }
-
-
-
-
-
-
   // operate(num1,num2,opSign);
 }
 
+function updateDisplay(num1,num2,opSign){
+ if (opSign === '+'){
+  display.textContent = num2;
+ } else {
+  display.textContent = num1;
+ }
+
+
+
+}
 
 
 
