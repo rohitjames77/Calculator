d7bbb6dcedceeefdc0555de0ded1e232814cc901
rohit james <rohitjames77@outlook.com>
2024-02-08 15:53:50 +0530

    Add getNum1andNum2 function

--- main2.js
+++ main2.js
@@ -1,45 +1,54 @@
 
 
 
-let inputArr = [];
+let rawInputArr = [];
 let display = document.querySelector('.display');
 let eventParent1 = document.querySelector('.inputParent');
 eventParent1.addEventListener('click',captureEvent1)
 function captureEvent1(event){
   let input = event.target.textContent;  
-     inputArr.push(input);
-     inputArr.join('');
-     sortInput(inputArr);
+     rawInputArr.push(input);
+     let inputArr=rawInputArr.join('');
+     getSignOp(inputArr);
     }
     
-function sortInput(inputArr){
-  let opIndex= inputArr.findIndex(element =>['+','-','x','/'].includes(element));
-  
-  if (opIndex !== -1){
-     let num1 = inputArr.slice(0,opIndex).join('');
-     console.log(num1);
+    function getSignOp(inputArr) {
+      let operatorSearchArr = ['+', '-', 'x', '/'];
+      for (let i =0; i< inputArr.length;i++){
+        if(inputArr.includes(operatorSearchArr[i])){
+          console.log('at index No '+ inputArr.indexOf(operatorSearchArr[i]));
+          let opIndex = inputArr.indexOf(operatorSearchArr[i]);
+          let opSign = inputArr.slice(opIndex,opIndex+1);
+          console.log(opSign);
+          let opSignIndex = inputArr.indexOf(opSign);
+          getNum1And2(inputArr,opSignIndex);
+          break;
+        }
+        
+      }
+    }
 
-     let num2 = inputArr.slice(opIndex +1).join('');
-     console.log(num2);
+    function getNum1And2(inputArr,opSignIndex){
+    let num1 = inputArr.slice(0, opSignIndex);
+    let num2 = inputArr.slice(opSignIndex + 1);
+    console.log('Number 1:', num1);
+    console.log('Number 2:', num2);
+      
+      }
 
-     let opSign = inputArr[opIndex];
-     console.log(opSign);
-   
-     updateDisplay(num1,num2,opSign);
-  }
-  // operate(num1,num2,opSign);
-}
-
-function updateDisplay(num1,num2,opSign){
- if (opSign === '+'){
-  display.textContent = num2;
- } else {
-  display.textContent = num1;
- }
+      
+      
 
 
 
-}
+
+
+
+
+      
+
+ 
+
 
 
 
