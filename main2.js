fa1f1cf9c4744b65f6ed1a5804cbb5f320eb5b8e
rohit james <rohitjames77@outlook.com>
2024-02-15 08:52:15 +0530

    Change result to string

--- main2.js
+++ main2.js
@@ -1,31 +1,32 @@
-let rawInputArr = [];
-let opSign;
-let input;
+var rawInputArr = [];
+var opSign;
+var input;
 
-let display = document.querySelector('.display');  
-let eventParent1 = document.querySelector('.inputParent');
+var display = document.querySelector('.display');  
+var eventParent1 = document.querySelector('.inputParent');
 eventParent1.addEventListener('click',captureEvent1)
 
 function captureEvent1(event){
-  let input = event.target.textContent;  
+  var input = event.target.textContent;  
     rawInputArr.push(input);
-     let inputArr=rawInputArr.join('');
+     var inputArr=rawInputArr.join('');
      display.textContent = inputArr;
   
-  getSignOp(inputArr);
-       
+  getSignOp(inputArr); 
+  
+  
          function getSignOp(inputArr) {
-      let operatorSearchArr = ['+', '-', 'x', '/'];
-      for (let i = 0; i< inputArr.length;i++){
+      var operatorSearchArr = ['+', '-', 'x', '/'];
+      for (var i = 0; i< inputArr.length;i++){
         if(inputArr.includes(operatorSearchArr[i])){
           // display.textContent = '';
           console.log('at index No '+ inputArr.indexOf(operatorSearchArr[i]));
-          let opIndex = inputArr.indexOf(operatorSearchArr[i]);
-          let opSign = inputArr.slice(opIndex,opIndex+1);
+          var opIndex = inputArr.indexOf(operatorSearchArr[i]);
+          var opSign = inputArr.slice(opIndex,opIndex+1);
           console.log(opSign);
-          let opSignIndex = inputArr.indexOf(opSign);
-          let num1 = inputArr.slice(0, opSignIndex);
-               let num2 = inputArr.slice(opSignIndex + 1 );
+          var opSignIndex = inputArr.indexOf(opSign);
+          var num1 = inputArr.slice(0, opSignIndex);
+               var num2 = inputArr.slice(opSignIndex + 1 );
                console.log('Number 2:', num2);  
                console.log('Number 1:', num1);
                display.textContent = num2;
@@ -39,8 +40,8 @@ function captureEvent1(event){
       function getEqualSign (inputArr){
         if (inputArr.includes('=')){
           display.textContent = '';
-          let equalIndex = inputArr.indexOf('=');
-          let equalSign = inputArr.slice(equalIndex,equalIndex+1)
+          var equalIndex = inputArr.indexOf('=');
+          var equalSign = inputArr.slice(equalIndex,equalIndex+1)
            console.log(equalIndex);
            console.log(equalSign);
       
@@ -52,10 +53,10 @@ function captureEvent1(event){
 }
 
 function operate (opSign,num1,num2){
-let result;
-let changedNum1 = parseFloat(num1);
+var result;
+var changedNum1 = parseFloat(num1);
 console.log(num2);
-let changedNum2;
+var changedNum2;
     if (num2.trim() !== '') {
         changedNum2 = parseFloat(num2);
         console.log(changedNum2);
@@ -71,6 +72,8 @@ console.log(changedNum2);
   result = changedNum1 / changedNum2;
 }
 display.textContent = result;
+var Str = result.toString();
+ rawInputArr.push(Str);
 
 }
 } 
