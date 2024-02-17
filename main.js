53d570e2bc8c123b46bdd2929ea88c475de5e638
rohit james <rohitjames77@outlook.com>
2024-02-17 16:30:31 +0530

    Add ultiple changes

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ var operator;
 
 function add (num1,num2){
 var result = num1 + num2;
-console.log(result);
 return result;
 }
 
@@ -19,26 +18,38 @@ add(2,2);
 
 function subtract (num1,num2){
   var result = num1 - num2;
-  console.log(result);
   return result;
 }
 subtract(2,2);
 
 function multiply (num1,num2){
   var result = num1 * num2;
-  console.log(result);
   return result;
 }
 multiply(2,4);
 
 function divide (num1,num2){
   var result = num1 / num2;
-  console.log(result);
   return result;
 }
 divide (16,8)
 
 
-function operate (operator,firstNo,secondNo){
-
+function operate (operator,num1,num2){
+if (operator === "+"){
+  var operation = add(num1,num2);
+  console.log(operation);
+  
+}else if( operator === '-'){
+  var operation = subtract(num1,num2);
+  console.log(operation);
+}else if( operator === '*'){
+  var operation = multiply(num1,num2);
+  console.log(operation);
+ 
+}else if( operator === '/'){
+  var operation = divide(num1,num2);
+  console.log(operation);
 }
+}
+operate('/',6,2);
