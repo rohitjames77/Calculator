64176368e4054591231ab941a11d98a120a1ab50
rohitjames <rohitjames77@outlook.com>
2024-02-21 19:08:49 +0530

    Complete till num1 and num2 further logic needed

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
-var num1;
-var num2;
-var operator;
-let display = document.querySelector('.display');
+
+var display = document.querySelector('.display');
 
 function calculation (){
   
-  let inputArray = [];
+  var inputArray = [];
 
   var numbers = document.querySelector('.numberInput');
 numbers.addEventListener('click', numClickInput);
 function numClickInput (event){
-let clickInput = event.target.textContent ;
+var clickInput = event.target.textContent ;
 inputArray.push(clickInput);
-let displayVal=display.textContent = inputArray.join(''); 
+var displayVal=display.textContent = inputArray.join(''); 
 sortInput (displayVal);
 }
 
 
-let mathSign = document.querySelector('.mathOpInput');
+var mathSign = document.querySelector('.mathOpInput');
 mathSign.addEventListener('click', mathClickInput );
 function mathClickInput (event){
-  let mathClick  = event.target.textContent;
+  var mathClick  = event.target.textContent;
   inputArray.push(mathClick);
-  let displayVal=display.textContent = inputArray.join(''); 
+  var displayVal=display.textContent = inputArray.join(''); 
 sortInput (displayVal);
   
 }
 
-let equalSign = document.querySelector('.equals');
+var equalSign = document.querySelector('.equals');
 equalSign.addEventListener('click',equalClickInput);
 function equalClickInput(event){
-let equalClick = event.target.textContent;
+var equalClick = event.target.textContent;
 inputArray.push(equalClick);
-let displayVal=display.textContent = inputArray.join(''); 
+var displayVal=display.textContent = inputArray.join(''); 
 sortInput (displayVal);
 
 }
 
-let ClearSign = document.querySelector('.clear');
+var ClearSign = document.querySelector('.clear');
 ClearSign.addEventListener('click',clearClickInput);
 
 function clearClickInput (event){
@@ -47,7 +45,7 @@ display.textContent = inputArray.join('');
 }
 }
 
-let powerSign = document.querySelector('.power');
+var powerSign = document.querySelector('.power');
 powerSign.addEventListener('click', powerInput);
 function powerInput (event){
 if (event.target.className = "power"){
@@ -55,71 +53,82 @@ if (event.target.className = "power"){
 }}
 
 
+  
+
+function sortInput (displayVal){
+var equalregex =  /=/;
+  var operatorPattern = /[+\-x\/\=]/g;
+  var operatorSearch =displayVal.search(operatorPattern);
+  console.log(operatorSearch);
+  var opMatch = displayVal.match(operatorPattern);
+  console.log(opMatch);
+ var operator = opMatch.pop();
+  console.log(operator); 
+  var num1str = displayVal.slice(0,operatorSearch);
+  console.log('num1',num1);
+  var num2str = displayVal.slice (operatorSearch+1);
+  console.log('num2',num2);
+  let equalOpSearch =  displayVal.search(equalregex) ;
+  console.log('equal',equalOpSearch);
+  let equalOp = displayVal.match(equalregex);
+  console.log('equalop',equalOp);
+  let equal = equalOp.pop();
+  console.log(equal);
+  var num1 = parseFloat(num1str);
+  var num2 = parseFloat(num2str);
+   operate(operator,num1,num2);
+}
+
 function add (num1,num2){
   var result = num1 + num2;
   return result;
   }
   
-  add(2,2);
+  add();
   
   function subtract (num1,num2){
     var result = num1 - num2;
     return result;
   }
-  subtract(2,2);
+  subtract();
   
   function multiply (num1,num2){
     var result = num1 * num2;
     return result;
   }
-  multiply(2,4);
+  multiply();
   
   function divide (num1,num2){
     var result = num1 / num2;
     return result;
   }
-  divide (16,8);
-
-  
-
-function sortInput (displayVal){
-
-  let operatorPattern = /[+\-*\/]/g;
-  if (displayVal.match(operatorPattern)){
-   
-
-  }
-
-
-
-
-}
-
-
-
-
-
-
-
+  divide ();
 
 function operate (operator,num1,num2){
-  if (operator === "+"){
+  if (operator === '+'){
     var operation = add(num1,num2);
-    console.log(operation);
+    console.log('result',operation);
+    display.textContent = operation;
     
   }else if( operator === '-'){
     var operation = subtract(num1,num2);
-    console.log(operation);
-  }else if( operator === '*'){
+    console.log('result',operation);
+    display.textContent = operation;
+
+  }else if( operator === 'x'){
     var operation = multiply(num1,num2);
-    console.log(operation);
+    console.log('result',operation);
+    display.textContent = operation;
    
   }else if( operator === '/'){
     var operation = divide(num1,num2);
-    console.log(operation);
+    console.log('result',operation);
+    display.textContent = operation;
+    
   }
+  
   }
-//  operate('+',7,2)
+
   
 
 }
