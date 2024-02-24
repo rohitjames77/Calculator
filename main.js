ec1b6d145c8aaa2f283502c04af043e302e8b2b3
rohit james <rohitjames77@outlook.com>
2024-02-24 18:26:44 +0530

    add eventlistener

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 
 let display = document.querySelector('.display');
-let numButtons = document.querySelector('.numInputsButtons');  
+let numButtons = document.querySelectorAll('.numInputsButtons');  
+console.log(numButtons);
 let clear = document.querySelector('.clear');
 let power = document.querySelector('.power');
 let equal = document.querySelector('.equals');
 let mathButtons = document.querySelector('.MathOpButtons');
 let clickInput = display.textContent;
 clickInput = '';
-
-function eventForAll (type,selector,callback){
-if ()
-}
 
 
 
@@ -40,3 +37,10 @@ function add (num1,num2){
   }
   divide (4,2);
 
+numButtons.forEach(applyEvent);
+
+function applyEvent(numButtons){
+  numButtons.addEventListener('click',(e)=>{
+  });
+}
+  
